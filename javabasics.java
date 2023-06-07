4b53d639d03530e7efb793d63badefd1dd2a0e1d
Anukool yadav <113781432+Anukoolyadav@users.noreply.github.com>
2023-06-07 22:20:41 +0530

    java
    add it

---
+++ javabasics.java
@@ -0,0 +1,16 @@
+public class javabasics {
+    public static void main(String args[]){
+        System.out.println("hello world\n");
+        System.out.println("hello world\n");
+        System.out.println("hello world\n");
+        System.out.println("hello world\n");
+
+
+
+
+
+
+
+    }
+}
+//boilerplate code
---
+++ pattern.java
@@ -0,0 +1,13 @@
+public class pattern {
+    public static void main(String args[]){
+        System.out.println("\n* * * * \n* * * \n* * \n* ");
+        System.out.print("hello O hello world world hello world hello");
+
+        
+    
+
+
+
+    }
+    
+}
