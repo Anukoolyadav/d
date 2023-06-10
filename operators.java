925a56bcb38079efd3f8eaf06d79c5cb69a6c2e8
Anukool yadav <captioncoolnew@gmail.com>
2023-06-10 13:17:56 +0530

    fj

---
+++ operators.java
@@ -0,0 +1,44 @@
+public class operators{
+    public static void main(String args[]){
+        int a =29;
+        int b=47;
+        int c;
+        // c=b%a;
+        // c=a+b;
+        // c=a-b;
+        // c=a*b;
+        // int d=++a;
+        // int d=a++;
+        // int d=--a;
+        // int d =a--;
+        // System.out.println(a);
+        // System.out.println(d);
+        // System.out.println(a==b);
+        // // System.out.println(a=!b);
+        // System.out.println(a!=b);
+        // System.out.println(a<b);
+        // System.out.println(a>b);
+        // System.out.println(a>=b);
+        // // System.out.println(a=>b);
+        // // System.out.println(a=<b);
+        // System.out.println(a<=b);
+
+        // System.out.println(c);
+        // a+=10;
+        // System.out.println(a-=10);
+        // System.out.println(a*10);
+        // System.out.println(a);
+        // System.out.print;
+        System.out.println((a<b) == (a>b));
+        System.out.println((a<b) || (a>b));
+        System.out.println((a<b) && (a>b));
+        boolean d;
+        d=((a<b) || (a>b));
+        System.out.println(d);
+        System.out.println(!d);
+        System.out.println(!!!!!!!!!!!!!!!!!!!!!!!!!!!!!(((a<b) || (a>b))));//depends on no. of ! not's in this line 
+
+
+
+    }
+}
