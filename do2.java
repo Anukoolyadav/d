10db1c425bc98653ca4413902f4d1eb09e1eba97
Anukool yadav <113781432+Anukoolyadav@users.noreply.github.com>
2023-06-13 22:06:19 +0530

    add'

---
+++ do2.java
@@ -0,0 +1,10 @@
+public class do2 {
+    public static void main(String args[]){
+        int i=1;
+        do{
+         System.out.print("hello world");
+         i++;
+        }while(i<=5);
+    }
+    
+}
