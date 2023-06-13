08c2f72e98c8d5f769305eecbda9da0515037ec3
Anukool yadav <113781432+Anukoolyadav@users.noreply.github.com>
2023-06-13 22:06:38 +0530

    doo

---
+++ loops.java
@@ -0,0 +1,16 @@
+public class loops{
+    public static void main(String[] args){
+
+        // System.out.println("Hello world");
+   
+        int n;
+        n=0;
+        while(n<100000){
+            System.out.println("Hello World");
+            // n++;
+            ++n;
+        }
+   
+    }
+}
+
