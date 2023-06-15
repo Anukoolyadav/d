d124567f994150fa00aff3fab8bdd6b9f7c79c5d
Anukool yadav <113781432+Anukoolyadav@users.noreply.github.com>
2023-06-15 17:28:18 +0530

    add to dqay 5

---
+++ day 5/ter_nary.java
@@ -0,0 +1,10 @@
+public class ter_nary {
+    public static void main(String args[]){
+        int i = (50>7)?1:0;
+        System.out.println(i);
+        int n= 221;
+        String x=(n%2==0)?"even":"odd";
+    System.out.println(x);
+    }
+    
+}
