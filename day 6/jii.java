ba8de019393a45d4c85f82151ac97a7b49c5e8bd
Anukool yadav <113781432+Anukoolyadav@users.noreply.github.com>
2023-06-18 22:04:40 +0530

    fdf

---
+++ day 6/jii.java
@@ -0,0 +1,14 @@
+public class jii {
+    public static int calculatorio(int num1,int num2){
+        int sum =num1+num2;
+        return sum;
+    
+    }
+    public static void main(String args[]){
+        int a =1;
+        int b=12;
+        int sum = calculatorio(a,b);
+        System.out.println("sum is "+sum);
+    }
+    
+}
