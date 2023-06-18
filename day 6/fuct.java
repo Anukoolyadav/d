d33589928ebdd8d48f217750c7919ecdc7597a31
Anukool yadav <113781432+Anukoolyadav@users.noreply.github.com>
2023-06-18 22:04:52 +0530

    dsfa

---
+++ day 6/fuct.java
@@ -0,0 +1,15 @@
+public class fuct{
+   public static void me(){
+    System.out.println("i am anukool");
+    System.out.println("like it was easy");
+   }
+   public static void live(){
+    System.out.println("players");
+   }
+    public static void main(String aegs[]){
+        me();
+        live();  
+          
+
+    }
+}
