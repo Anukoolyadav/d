d9123b8d7638d8c4dea7c8bbb5627aede59096f0
Anukool yadav <113781432+Anukoolyadav@users.noreply.github.com>
2023-06-19 21:17:19 +0530

    Add files via upload

---
+++ day 6/nep.java
@@ -0,0 +1,11 @@
+public class nep {
+    public static void cal(){
+        
+    }
+    public static void main(String args[]){
+        System.out.println("HI");
+        
+        
+    }
+    
+}
