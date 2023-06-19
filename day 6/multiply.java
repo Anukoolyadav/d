d24a10adaefb454790b129dfbeaad290f8550caa
Anukool yadav <113781432+Anukoolyadav@users.noreply.github.com>
2023-06-19 21:17:36 +0530

    Add files via upload

---
+++ day 6/multiply.java
@@ -0,0 +1,15 @@
+public class multiply {
+    public static int mul(int a,int b){
+        int m = a*b;
+        return m;
+    }
+    public static void main(String args[]){
+        int a =4;
+        int b =7;
+        int x=mul(a, b);
+        System.out.println("mul : "+x);
+        int q=mul(4, 5);
+        System.out.println("q is : "+q);
+    }
+    
+}
