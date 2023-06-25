6ce6be1c2a545c9d4cbb26d311f03612e5e2a0fb
Anukool yadav <113781432+Anukoolyadav@users.noreply.github.com>
2023-06-25 20:47:57 +0530

    Add files via upload

---
+++ Marks.java
@@ -0,0 +1,9 @@
+public class Marks{
+    public static void main(String args[]){
+        int marks[]={12,25,13};
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+        System.out.println(marks.length);
+    }
+}
