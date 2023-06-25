5384b6b0df9bac680d3d8a9aba5c8fe79855efdf
Anukool yadav <113781432+Anukoolyadav@users.noreply.github.com>
2023-06-25 20:47:42 +0530

    Add files via upload

---
+++ MarksScanner.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class MarksScanner {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int marks[]=new int[40];
+        marks[0]=sc.nextInt();
+        System.out.println(marks[0]);                 
+    
+    }
+    
+}
