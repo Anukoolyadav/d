9eb70dd3b4af4f905aa32894916add9a52675ef4
Anukool yadav <113781432+Anukoolyadav@users.noreply.github.com>
2023-06-28 15:22:09 +0530

    Add files via upload

---
+++ summary_chapter1.py
@@ -0,0 +1,31 @@
+# print function 
+print( "hello")
+# escape sequence
+print("hello \" this is me")
+print("line A \nline B")
+print("line A \tline B")
+print("this is \\\\ double backslash")
+
+# escape sequence as a normal text
+print(r"hey \"'''''")
+print(r"line A \nline B")
+
+# python as a calculator
+print(2+6)
+print(2-7)
+print(4/4)
+print(4//4)
+print(40%7)
+print(4**4)
+print((2*3)*6%6+6)
+
+# variables
+x="name"
+print(x)
+x="anu"
+print(x)
+# first letter will start only with alphabets or hyphen
+# user_one  snake case writing
+# userOne   camel case writing
+# THIS IS THE END OF CHAPTER ONE
+print("\U0001F60D")
