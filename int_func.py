c1a3f3d7c17894cc16b458609e925a611c03cb5c
Anukool yadav <113781432+Anukoolyadav@users.noreply.github.com>
2023-07-01 21:01:54 +0530

    Add files via upload

---
+++ int_func.py
@@ -0,0 +1,6 @@
+number_one=int(input("Enter first number:"))
+number_two=int(input("Enter second number:"))
+total=number_one+number_two
+# print("total is :"+str(total))
+# print(total)
+print(float(total))
