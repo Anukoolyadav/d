e6071dcb87b8f59098a6aa7866f6c65c3c9ea503
Anukool yadav <113781432+Anukoolyadav@users.noreply.github.com>
2023-07-04 20:45:01 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,11 @@
+print(2+5)
+print(2+4*6)
+print(2/4)  #float division 
+print(2//4) #integer division
+print(2**4) #exponent
+print(2**0.5)
+print(round(2**0.5,5))
+print(2%4)
+print(4%2)
+a='anukool'
+
